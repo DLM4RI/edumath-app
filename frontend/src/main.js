a5b6bb51e92f0f144de26d3202c98db234bc1984
DLM4RI <mario.andres.castro.9.4@gmail.com>
2026-05-09 20:24:31 -0500

    Repair version

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,6 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import App from "./App.vue";
-import router from "./router";
-import vuetify from "./plugins/vuetify";
-import "./style.css";
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-const app = createApp(App);
-const pinia = createPinia();
+createApp(App).mount('#app')
 
-app.use(pinia);
-app.use(router);
-app.use(vuetify);
-
-app.mount("#app");
